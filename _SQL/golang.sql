b89db0a83ff2701857fdc85ea3bab95520118c4c
Dmitry <kuchura.d@gmail.com>
2017-10-27 15:47:44 +0300

    Update golang.sql

--- _SQL/golang.sql
+++ _SQL/golang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 25 2017 г., 16:42
+-- Время создания: Окт 27 2017 г., 15:47
 -- Версия сервера: 5.7.19
 -- Версия PHP: 7.1.7
 
@@ -33,8 +33,8 @@ CREATE TABLE `domains` (
   `name` varchar(150) NOT NULL,
   `url` varchar(150) NOT NULL,
   `status` tinyint(1) NOT NULL,
-  `created_at` int(11) DEFAULT NULL,
-  `updated_at` int(11) DEFAULT NULL
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -42,7 +42,11 @@ CREATE TABLE `domains` (
 --
 
 INSERT INTO `domains` (`id`, `name`, `url`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'tirmarket.com.ua', 'http://tirmarket.com.ua', 1, NULL, NULL);
+(1, 'tirmarket.com.ua', 'http://tirmarket.com.ua', 1, '2017-10-27 11:27:25', '2017-10-27 11:27:25'),
+(2, 'wezom.com.ua', 'https://wezom.com.ua', 1, '2017-10-27 11:27:25', '2017-10-27 11:27:25'),
+(3, 'infocorpus.wezom.net', 'http://infocorpus.wezom.net', 1, '2017-10-27 11:27:25', '2017-10-27 11:27:25'),
+(4, 'lester.com.ua', 'http://lester.com.ua', 1, '2017-10-27 11:27:25', '2017-10-27 11:27:25'),
+(5, 'gmail.com', 'https://gmail.com', 1, '2017-10-27 11:38:13', '2017-10-27 11:38:13');
 
 -- --------------------------------------------------------
 
@@ -99,7 +103,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `token`, `name`, `role`, `status`) VALUES
-(1, 'kuchura.d@gmail.com', '4ceb2016943ef079185ad57a1380113d', 'a1aa10018d6111e780bb000c2', 'Dmitry', 0, 1);
+(1, 'kuchura.d@gmail.com', '4ceb2016943ef079185ad57a1380113d', 'a1aa10018d6111e780bb000c2', 'Dmitry', 0, 1),
+(2, 'ivan@gmail.com', '9273137617fc5e5e2f8749a12af17d14', 'ddkiEqaYR7bjl910ZLFFTkJ8a', 'Ivan', 0, 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -137,7 +142,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `domains`
 --
 ALTER TABLE `domains`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT для таблицы `ftp`
 --
@@ -152,7 +157,7 @@ ALTER TABLE `mysql`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
