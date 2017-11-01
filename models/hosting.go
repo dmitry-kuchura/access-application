e623566f026d1063e59c49e4bf933bf98de4850c
Dmitry <kuchura.d@gmail.com>
2017-11-01 16:51:40 +0200

    Update hosting.go

---
+++ models/hosting.go
@@ -0,0 +1,10 @@
+package models
+
+type Hosting struct {
+	ID        int    `form:"id" json:"id"`
+	Name      string `form:"name" json:"name"`
+	Url       string `form:"url" json:"url"`
+	Status    int    `form:"status" json:"status"`
+	CreatedAt string `form:"created_at" json:"created_at"`
+	UpdatedAt string `form:"updated_at" json:"updated_at"`
+}
