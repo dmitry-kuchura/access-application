7c4bc1127421486e37176e8ee5cfa43e999cfe40
Dmitry <kuchura.d@gmail.com>
2017-11-01 16:53:36 +0200

    Update admin.go and hosting.go

--- models/admin.go
+++ models/admin.go
@@ -4,6 +4,8 @@ type Hosting struct {
 	ID        int    `form:"id" json:"id"`
 	Name      string `form:"name" json:"name"`
 	Url       string `form:"url" json:"url"`
+	Login     string `form:"login" json:"login"`
+	Password  string `form:"password" json:"password"`
 	Status    int    `form:"status" json:"status"`
 	CreatedAt string `form:"created_at" json:"created_at"`
 	UpdatedAt string `form:"updated_at" json:"updated_at"`
